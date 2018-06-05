e34bb45b5c6e3f6bda9e7c8b0b32040389e1f366
alex <gongqiang@laoyuegou.com>
2018-06-05 17:41:22 +0800

    fix

--- application/controllers/Index.php
+++ application/controllers/Index.php
@@ -1,24 +1,11 @@
 <?php
+
 class IndexController extends \Yaf_Controller_Abstract {
     public function indexAction() {//默认Action
-        $id = $_GET['id'];
-        $d  = $_GET['d'];
-        var_export($id);
-        var_export($d);
-        $res = [];
-//        $db  = new PDO("mysql:dbname=test;host=127.0.0.1", "root", "root");
-//        foreach (range(1, 5) as $k => $v) {
-//            $res[] = $db->query("select * from user where id =$v")->fetchAll(\PDO::FETCH_ASSOC);
-//        }
-//        print_r($res);
         $this->getView()->assign("content", "Hello World");
     }
     
     public function testAction() {//默认Action
-        $id = $_GET['id'];
-        $d  = $_GET['d'];
-        var_export($id);
-        var_export($d);
         $this->getView()->assign("content", "Hello World");
     }
 }
--- application/controllers/Test.php
+++ application/controllers/Test.php
@@ -1,30 +1,30 @@
 <?php
 
 class TestController extends \Yaf_Controller_Abstract {
+    use \cat\lib\ApiControllerTrait;
+    
     public function init() {
         if ($this->getRequest()->isXmlHttpRequest()) {
             Yaf_Dispatcher::getInstance()->autoRender(FALSE);
         }
-
+        
         Yaf_Dispatcher::getInstance()->autoRender(FALSE);
-
+        
     }
-
+    
     public function testAction() {
-        $a = ["data" => 555];
-        echo json_encode($a);
+        $get  = $this->getQuery();
+        $file = $this->getFiles();
+        $post = $this->getPost();
+        print_r($file);
+        print_r($post);
+        print_r($get);
         return;
     }
-
+    
     public function indexAction() {//默认Action
-//        \cat\application\library\Handle::test();
-        $ip = swoole_get_local_ip();
-//        print_r($ip);
-        $a = ["data" => 555];
-        echo json_encode($a);
-        return;
-
-        $this->getView()->assign("content", "Hello World");
-//        $this->getResponse()->setBody("Hello World");
+        
     }
+    
+    
 }
